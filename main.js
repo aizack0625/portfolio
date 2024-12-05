7888705c6a1e1b2414067f23812aca850c1879ac
aizack0625 <69981290+aizack0625@users.noreply.github.com>
2024-12-05 20:49:51 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+
+// // ダークモード
+// const btn = document.querySelector("#btn-dark-mode");
+// //チェックボックス切り替え判定
+// btn.addEventListener("change", () => {
+//   if (btn.checked === true) {
+//     document.body.classList.remove('light-mode');
+//     document.body.classList.add('dark-mode');
+//   }else {
+//     document.body.classList.remove('dark-mode');
+//     document.body.classList.add('light-mode');
+//   }
+// });
+
+
